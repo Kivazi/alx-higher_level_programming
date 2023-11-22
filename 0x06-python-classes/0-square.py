b70369a6e4e8d8a08bb0fb5a576de7f271475ab2
peter kateeba <petrkat8ba@gmail.com>
2023-11-22 08:08:00 -0800

    square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 """an empty class Square"""
 class Square:
+     """Represent a square."""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 """initializing a new Square with size"""
 class Square:
+    
     def __init__(self,size):
+         """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
         slef.__size=size
