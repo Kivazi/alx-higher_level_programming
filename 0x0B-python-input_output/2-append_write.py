f2277bb4039bd696c6b25d56b72d0a8896de4887
peter kateeba <petrkat8ba@gmail.com>
2024-02-06 07:19:00 -0800

    Append to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a file-writing function."""
+
+
+def append_write(filename="", text=""):
+    """Append a string to a UTF8 text file.
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to append to the file.
+    Returns:
+        The number of characters appended.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
