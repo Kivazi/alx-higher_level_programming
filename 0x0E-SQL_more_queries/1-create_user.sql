b176cdde7cab756bfdcfdbb37dc994dcb1a05969
kivazi <petrkateeb4@gmail.com>
2024-04-07 07:20:39 -0700

    update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- script that creates the MySQL server user user_0d_1 with privilages
 CREATE USER IF NOT EXIST 'user_0d_1@localhost' IDENTIFIED BY user_0d_1_pwd;
-GRANT PRIVILEGES ON *.* TO user_0d_1;
+GRANT PRIVILEGES ON *.* TO user_0d_1@localhost;
