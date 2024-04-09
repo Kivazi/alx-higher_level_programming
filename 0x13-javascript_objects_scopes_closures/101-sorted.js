a06912e5132bd996cf57e4d252adad6e47e2ee6a
peter kateeba <petrkat8ba@gmail.com>
2024-04-09 10:01:37 -0700

    update

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,4 +1,13 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-console.log(list);
-console.log(list.map((x, i) => x * i));
+const dict = require('./101-data').dict;
+const dKeys = Object.keys(dict);
+const values = Object.values(dict);
+let matched;
+const result = {};
+// loop over the values
+for (let i = 0; i < values.length; i++) {
+  result[JSON.stringify(values[i])] = [];
+  matched = dKeys.filter(key => dict[key] === values[i]);
+  matched.forEach(item => result[JSON.stringify(values[i])].push(item));
+}
+console.log(result);
